1dc7d3ef8935164a5ca8ef13f0d0f3d1da19d252
walaa soudy <77520251+walaasoudy@users.noreply.github.com>
2024-11-07 11:55:58 +0200

    first commit

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,31 @@
+// backend/routes/taskRoutes.js
+const express = require('express');
+const router = express.Router();
+const Task = require('../models/Task');
+
+// Get all tasks
+router.get('/', async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+// Create a new task
+router.post('/', async (req, res) => {
+  const newTask = new Task(req.body);
+  await newTask.save();
+  res.status(201).json(newTask);
+});
+
+// Update a task
+router.put('/:id', async (req, res) => {
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(task);
+});
+
+// Delete a task
+router.delete('/:id', async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Task deleted' });
+});
+
+module.exports = router;
